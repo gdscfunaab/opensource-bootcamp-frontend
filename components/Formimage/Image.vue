63833cf706f2ab04288dba9d76a308baeeafdf79
Degreatisaac <okeisaacoluwasesangreat1@gmail.com>
2022-02-24 03:04:18 -0800

    Registration Form

--- components/Formimage/Image.vue
+++ components/Formimage/Image.vue
@@ -1,3 +1,11 @@
 <template>
-<div class="man"><img src="broke.png" alt=""></div>
+<div class="man"><img src="https://huntr.netlify.app/img/background.888e624f.jpg" alt="" width="700" height="700"/></div>
 </template>
+<style scoped>
+.man{
+    position: relative;
+    bottom: 500px;
+    right: 105px;
+    opacity: px;
+}
+</style>
